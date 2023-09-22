3c09deaa909c5e5bd19ebbbd37757598c9eb3a8b
Natalia Melnik <nataliamelnik138@gmail.com>
2023-09-22 21:19:23 +0600

    Добавлены в административную панель модели

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailings.models import MailingSettings, Clients, Messages
+
+
+@admin.register(MailingSettings)
+class MailingSettingsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'start_date', 'end_date', 'status', )
+    list_filter = ('status',)
+
+
+@admin.register(Clients)
+class ClientsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'fullname', 'email', )
+
+
+@admin.register(Messages)
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'subject',)
+    search_fields = ('subject', 'content',)
