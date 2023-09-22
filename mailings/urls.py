14361606531813c84390d14cb9e3a306ac9e908c
Natalia Melnik <nataliamelnik138@gmail.com>
2023-09-22 21:21:29 +0600

    Добавлены urls

---
+++ mailings/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from mailings.apps import MailingsConfig
+from mailings.views import MailingSettingsListView, MailingSettingsCreateView, MailingSettingsUpdateView, \
+    MailingSettingsDetailView, MailingSettingsDeleteView, ClientsDeleteView, ClientsDetailView, ClientsUpdateView, \
+    ClientsCreateView, ClientsListView
+
+app_name = MailingsConfig.name
+
+
+urlpatterns = [
+    path('', MailingSettingsListView.as_view(), name='list'),
+    path('create/', MailingSettingsCreateView.as_view(), name='create'),
+    path('edit/<int:pk>', MailingSettingsUpdateView.as_view(), name='edit'),
+    path('view/<int:pk>', MailingSettingsDetailView.as_view(), name='view'),
+    path('delete/<int:pk>', MailingSettingsDeleteView.as_view(), name='delete'),
+    path('list_client/', ClientsListView.as_view(), name='list_client'),
+    path('create_client/', ClientsCreateView.as_view(), name='create_client'),
+    path('edit_client/<int:pk>', ClientsUpdateView.as_view(), name='edit_client'),
+    path('view_client/<int:pk>', ClientsDetailView.as_view(), name='view_client'),
+    path('delete_client/<int:pk>', ClientsDeleteView.as_view(), name='delete_client'),
+]
